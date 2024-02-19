615754d135aa8016d196065e4d1ba71b30476a85
Rimantas <belauzas@gmail.com>
2024-02-19 09:03:16 +0200

    querySelector

--- js/style.js
+++ js/style.js
@@ -18,3 +18,25 @@ h1DOM.classList.add('italic');
 h1DOM.classList.remove('italic');
 
 console.log(h1DOM.classList);
+
+const contentDOM = document
+    .getElementsByTagName('ul')[0]
+    .getElementsByTagName('li');
+console.log(contentDOM);
+
+for (const liDOM of contentDOM) {
+    liDOM.textContent += ' (1)';
+}
+
+const contentDOM2 = document
+    .querySelector('.list > li');
+console.log(contentDOM2);
+
+const contentDOM3 = document
+    .querySelectorAll('.list')[1]
+    .querySelectorAll('li');
+console.log(contentDOM3);
+
+// .getElementById()
+// .querySelector()
+// .querySelectorAll()
